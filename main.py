4f05a86acb13761c8ccc59844a160358af117dfa
Diogenes Ferreira <95397773+diogenesferreirabr@users.noreply.github.com>
2023-03-27 23:36:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print('hello world ')
+nome = input("digite seu nome:")
+print('olá', nome,  'seja bem vindo.')
+idade = input('digite sua idade:')
+print('você tem',idade, 'anos.')
+print ('e foi um prazer te conhecer.')
+
+ano = int(input('digite o ano em que nasceu :'))
+resp = 2023 - ano
+print('sua idade', resp)
